4bad19f6c4efb4f9fadf91d39d641f36c6c93bf3
eneridahunda <eneridahunda@gmail.com>
2022-05-30 17:17:37 +0200

    Add project files.

---
+++ Frågor.cs
@@ -0,0 +1,84 @@
+﻿//Fråga 1
+
+//En stack är en datastruktur där man bara kan lägga 
+//till nya element överst, och ta bort element överst, d.v.s. i omvänd ordning som 
+//man la till dem (som på en stapel tallrikar). Stacken, i bestämd form, är en 
+//sådan datastruktur som programmet använder när det körs för att lagra data som 
+//det (programmet) behöver.
+
+
+
+
+//int add(int x, int y)
+//{
+
+//    int result = x + y;
+
+//    return result;
+
+//}
+
+//void print_times(int x, int times)
+//{
+
+//    for (int i = 0; i < times; i++)
+
+//    {
+
+//        Console.WriteLine(x);
+
+//    }
+
+//}
+
+//void add_and_print_3_times(int a, int b)
+//{
+
+//    int sum = add(a, b);
+
+//    print_times(sum, 3);
+
+//}
+
+
+
+//static int Main(string[] args)
+//{
+
+//    add_and_print_3_times(3, 17);
+
+//    return 0;
+
+//}
+
+//Så hur används heapen? 
+
+//Om en stackframe som refererar till ett objekt tas bort (för 
+//att vi är klara med den metoden) så förlorar vi referensen till objektet om vi 
+//inte hade sparat undan den nånstans. Om vi nu inte har kvar några referenser 
+//till objektet så kommer objektet så småningom att städas undan av garbage 
+//collectorn. 
+
+ 
+
+ 
+
+ 
+
+//Fråga 2 
+
+ 
+
+//Värdedatatyper: I C# kommer värdedatatyperna att lagra variabelvärdet direkt i minnet och det kommer också att acceptera både signerade och osignerade bokstaver. Den härledda klassen för dessa datatyper är System.ValueType . 
+
+//Referensdatatyper: Referensdatatyperna kommer att innehålla en minnesadress med variabelt värde eftersom referenstyperna inte lagrar variabelvärdet direkt i minnet. De inbyggda referenstyperna är sträng, objekt. 
+
+//Skillnaden är att en värdetyp lagrar sitt värde direkt, medan en referenstyp lagrar en referens till värdet. Värdetyper i C# är i princip samma som enkla typer (heltal, flytande men inte pekare eller referenser). 
+
+ 
+
+//Fråga 3 
+
+//I det första fallet får X värdet 3. Sedan tar en ny variabel som initieras med Y och får värdet 3 efter att den är lika med X. Då är Y lika med 4 men värdet på X kommer att förbli 3 eftersom det i en STACKEN spelar roll att tilldela värden. 
+
+//I det andra fallet har vi att göra med ett HEAPEN, därför blir det slutliga resultatet 4. 
---
+++ Program.cs
@@ -0,0 +1,306 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkalProj_Datastrukturer_Minne
+{
+    class Program
+    {
+        /// <summary>
+        /// The main method, vill handle the menues for the program
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main()
+        {
+
+            while (true)
+            {
+                Console.WriteLine("Please navigate through the menu by inputting the number \n(1, 2, 3 ,4, 0) of your choice"
+                    + "\n1. Examine a List"
+                    + "\n2. Examine a Queue"
+                    + "\n3. Examine a Stack"
+                    + "\n4. CheckParanthesis"
+                    + "\n0. Exit the application");
+                char input = ' ' ; //Creates the character input to be used with the switch-case below.
+                try
+                {
+                    input = Console.ReadLine()![0]; //Tries to set input to the first char in an input line
+                }
+                catch (IndexOutOfRangeException) //If the input line is empty, we ask the users for some input.
+                {
+                    Console.Clear();
+                    Console.WriteLine("Please enter some input!");
+                }
+                switch (input)
+                {
+                    case '1':
+                        ExamineList();
+                        break;
+                    case '2':
+                        ExamineQueue();
+                        break;
+                    case '3':
+                        ExamineStack();
+                        break;
+                    case '4':
+                        CheckParanthesis();
+                        break;
+                    /*
+                     * Extend the menu to include the recursive 
+                     * and iterative exercises.
+                     */
+                    case '0':
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Please enter some valid input (0, 1, 2, 3, 4)");
+                        break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Examines the datastructure List
+        /// </summary>
+        static void ExamineList()
+        {
+            /*
+             * Loop this method untill the user inputs something to exit to main menue.
+             * Create a switch statement with cases '+' and '-'
+             * '+': Add the rest of the input to the list (The user could write +Adam and "Adam" would be added to the list)
+             * '-': Remove the rest of the input from the list (The user could write -Adam and "Adam" would be removed from the list)
+             * In both cases, look at the count and capacity of the list
+             * As a default case, tell them to use only + or -
+             * Below you can see some inspirational code to begin working.
+            */
+
+            List<string> theList = new List<string>();
+            string input = Console.ReadLine();
+            char nav = input[0];
+           // string value = input.substring(1);
+
+            switch(nav){
+
+                case '1':
+                    AddInput();
+                break;
+                case '2':
+                    RemoveInput();
+                break;
+                default:
+                    Console.WriteLine("Please enter only (+) or (-)");
+                    break;
+
+            }
+        }
+
+        private static void RemoveInput()
+        {
+            List<string> theList = new List<string>();
+            Console.WriteLine("Please remove an input.");
+            string input = Console.ReadLine();
+            theList.Remove(input);
+
+            while (!String.IsNullOrEmpty(input))
+            {
+
+                Console.WriteLine("Please enter the input you want to remove: ");
+                input = Console.ReadLine();
+
+            }
+
+
+            if (String.IsNullOrEmpty(input))
+            {
+                Console.WriteLine("The input you have entered is: " + " " + input);
+                theList.Remove(input);
+                foreach (string value in theList)
+                {
+                    Console.WriteLine("The input that was removed from the list is : " + " " + value);
+                }
+                Console.ReadLine();
+            }
+        }
+
+        private static void AddInput()
+        {
+
+
+            List<string> theList = new List<string>();
+            Console.WriteLine("Please enter your input.");
+            string input = Console.ReadLine();
+            theList.Add(input);
+
+            while (!String.IsNullOrEmpty(input))
+            {
+
+                Console.WriteLine("Please enter another integer: ");
+                input = Console.ReadLine();
+
+            }
+
+
+            if (String.IsNullOrEmpty(input))
+            {
+                Console.WriteLine("The input you have entered is: " + " " + input);
+                theList.Add(input);
+                foreach (string value in theList)
+                {
+                    Console.WriteLine("The input that was added to the list is : " + " " + value);
+                }
+                Console.ReadLine();
+            }
+
+        }
+
+        /// <summary>
+        /// Examines the datastructure Queue
+        /// </summary>
+        static void ExamineQueue()
+        {
+            // Loop this method untill the user inputs something to exit to main menue.
+             
+            int num = 0;
+            string userChoice = string.Empty;
+            do
+            {
+                
+                Console.Write("Enter the number of your choice: ");
+                if (num == 0)
+                    break;
+                else
+                    userChoice = Console.ReadLine();
+
+            }
+            while (Int32.TryParse(userChoice, out num)); //checks to see if the input is a number
+            if (userChoice == "0")
+            {
+                Environment.Exit(0);
+            }
+
+            Console.WriteLine("Choice = " + userChoice);
+
+            // Create a switch with cases to enqueue items or dequeue items
+
+            Queue q = new Queue();
+
+            char input = ' ';
+            //switch (input)
+            //{
+
+            //    case ' 1 ':
+            //        q.Enqueue(AddInput);
+            //        break;
+            //    case ' 2 ':
+            //        q.Dequeue();
+            //        break;
+            //    default:
+            //        Environment.Exit(0);
+            //        break;
+
+            //}
+
+            q.Enqueue("Kallle");
+            q.Enqueue("Greta");
+            q.Enqueue("Stina");
+            
+
+
+            Console.WriteLine("Current queue: ");
+            foreach (string s in q) Console.Write(s + " ");
+          
+            Console.WriteLine();
+            q.Enqueue("Olle");
+
+            Console.WriteLine("Current queue: ");
+            foreach (string s in q) Console.Write(s + " ");
+
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+
+        // Make sure to look at the queue after Enqueueing and Dequeueing to see how it behaves
+
+        /// <summary>
+        /// Examines the datastructure Stack
+        /// </summary>
+        static void ExamineStack()
+        {
+            
+              //Loop this method until the user inputs something to exit to main menue.
+              
+              int num = 0;
+            string userChoice = string.Empty;
+            do
+            {
+                
+                Console.Write("Enter the number of your choice: ");
+                if (num == 0)
+                    break;
+                else
+                    userChoice = Console.ReadLine();
+
+            }
+            while (Int32.TryParse(userChoice, out num)); //checks to see if the input is a number
+            if (userChoice == "0")
+            {
+                Environment.Exit(0);
+            }
+
+            Console.WriteLine("Choice = " + userChoice);
+
+            // Create a switch with cases to push or pop items
+
+            Stack st = new Stack();
+
+            string input = "";
+
+            while (!String.IsNullOrEmpty(input))
+
+            {
+                Console.WriteLine("Enter a name:");
+
+                st.Pop();
+               
+                input = Console.ReadLine();
+
+            }
+
+            st.Push("Kalle");
+            st.Push("Greta");
+            st.Push("Stina");
+
+            foreach (Object obj in st)
+            {
+                Console.WriteLine(obj);
+            }
+
+
+            Console.WriteLine();
+            Console.WriteLine("The number of names in the stack " + st.Count);
+             Console.ReadKey();
+
+            // Make sure to look at the stack after pushing and and poping to see how it behaves
+
+        }
+
+        static void CheckParanthesis()
+        {
+            /*
+             * Use this method to check if the paranthesis in a string is Correct or incorrect.
+             * Example of correct: (()), {}, [({})],  List<int> list = new List<int>() { 1, 2, 3, 4 };
+             * Example of incorrect: (()]), [), {[()}],  List<int> list = new List<int>() { 1, 2, 3, 4 );
+             */
+
+        }
+
+    }
+
+    
+}
+
+
+
